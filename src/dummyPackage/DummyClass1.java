0a3f809fdb2900f3c0a979e53ad8e0ada3707a14
rizupep <mohamed.rizwan93@gmail.com>
2018-04-21 18:07:48 +0530

    first commit

---
+++ src/dummyPackage/DummyClass1.java
@@ -0,0 +1,28 @@
+package dummyPackage;
+
+public class DummyClass1 
+{
+	static int n;
+ public static void main(String[] args) throws Exception {
+	for(int i=0;i<5;i++)
+	{
+		
+		for ( int j=5 ; j!=i ; j--){
+			//System.out.print("  ");
+			System.out.print("j");	
+		}
+		System.out.println("1");
+		
+		
+		//System.out.println(n);
+		
+	}
+	 }
+
+public void testReserve1(String testcaseid) throws Exception 
+{		
+	
+	System.out.println("testinh");
+}
+}
+
---
+++ src/dummyPackage/DummyClass2.java
@@ -0,0 +1,12 @@
+package dummyPackage;
+
+public class DummyClass2 extends DummyClass1
+{
+	public void testReserve(String testcaseid) throws Exception 
+	{		
+		
+		testReserve1(null);
+		System.out.println("Dummy Class 22");
+	}
+	
+}
